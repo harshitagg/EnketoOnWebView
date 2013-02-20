f7364355b4ce4dda4c1a9920c9830b1ca73f07a6
Harshit Agarwal <harshita@thoughtworks.com>
2013-02-20 17:22:24 +0530

    Harshit | Parsing xml to get model and form.

--- src/org/ei/enketoonwebview/activity/HomeActivity.java
+++ src/org/ei/enketoonwebview/activity/HomeActivity.java
@@ -3,26 +3,45 @@ package org.ei.enketoonwebview.activity;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.EditText;
+import android.widget.Toast;
 import org.ei.enketoonwebview.R;
 import org.ei.enketoonwebview.agent.HttpAgent;
-
-import java.io.InputStream;
+import org.ei.enketoonwebview.service.EnketoFormDataFetchService;
 
 public class HomeActivity extends Activity {
+
+    private HttpAgent httpAgent;
+    private EnketoFormDataFetchService formDataFetchService;
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.home_layout);
+        initializeView();
+
+        httpAgent = new HttpAgent();
+        formDataFetchService = new EnketoFormDataFetchService(httpAgent);
+    }
+
+    private void initializeView() {
+        editText = (EditText) findViewById(R.id.editText);
     }
 
     public void testForm(View view) {
-        final HttpAgent httpAgent = new HttpAgent();
-        new AsyncTask<String, Void, InputStream>() {
+        String url = editText.getText().toString();
+        if (!URLUtil.isHttpUrl(url)) {
+            Toast.makeText(this, "Invalid URL, please check!!", Toast.LENGTH_SHORT).show();
+        }
+        new AsyncTask<String, Void, Pair>() {
             @Override
-            protected InputStream doInBackground(String... urls) {
-                return httpAgent.fetch(urls[0]);
+            protected Pair doInBackground(String... urls) {
+                return formDataFetchService.fetch(urls[0]);
             }
         }.execute("http://formhub.org/drishti_forms&form_id=ANC_v701_Copy");
     }
---
+++ src/org/ei/enketoonwebview/service/EnketoFormDataFetchService.java
@@ -0,0 +1,53 @@
+package org.ei.enketoonwebview.service;
+
+import android.util.Pair;
+import org.ei.enketoonwebview.agent.HttpAgent;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+public class EnketoFormDataFetchService {
+    private HttpAgent httpAgent;
+
+    public EnketoFormDataFetchService(HttpAgent httpAgent) {
+        this.httpAgent = httpAgent;
+    }
+
+    public Pair<String, String> fetch(String url) {
+        InputStream inputStream = httpAgent.fetch(url);
+        try {
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            org.w3c.dom.Document document1 = documentBuilder.parse(inputStream);
+            org.w3c.dom.Element root = document1.getDocumentElement();
+            NodeList nodes = root.getChildNodes();
+
+            String model = nodeToString(nodes.item(0));
+            String form = nodeToString(nodes.item(1));
+            System.out.println(model);
+            System.out.println(form);
+            return new Pair<String, String>(model, form);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String nodeToString(Node node) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(node), new StreamResult(writer));
+        return writer.getBuffer().toString().replaceAll("\n|\r|\t", "");
+    }
+}
